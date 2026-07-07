16405ea2e6201f3a3a40d94a4712e497ecf4e863
rafizul896 <rafizulislam896@gmail.com>
2026-07-07 14:05:25 +0600

    add: Login and register page basic UI

---
+++ src/components/modules/Auth/LoginForm.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+const LoginForm = () => {
+  return (
+    <form>
+      <FieldGroup>
+        <div className="grid grid-cols-1 gap-4">
+          {/* Email */}
+          <Field>
+            <FieldLabel htmlFor="email">Email</FieldLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="m@example.com"
+              //   required
+            />
+          </Field>
+
+          {/* Password */}
+          <Field>
+            <div className="flex  justify-between">
+              <FieldLabel htmlFor="password">Password</FieldLabel>
+              <a
+                href="/forget-password"
+                className="text-blue-600 hover:underline"
+              >
+                Forgot password?
+              </a>
+            </div>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Enter your password"
+              //   required
+            />
+          </Field>
+        </div>
+        <FieldGroup className="mt-4">
+          <Field>
+            <Button type="submit">Login</Button>
+
+            <FieldDescription className="px-6 text-center">
+              Don&apos;t have an account?{" "}
+              <a href="/register" className="text-blue-600 hover:underline">
+                Sign up
+              </a>
+            </FieldDescription>
+          </Field>
+        </FieldGroup>
+      </FieldGroup>
+    </form>
+  );
+};
+
+export default LoginForm;
